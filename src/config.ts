5e44e7dfa8a59eddd15947ce282cc8fdc70fb195
FuSoraS <developer.fusoras@gmail.com>
2026-07-29 17:55:10 -0400

    refactor: prettier format

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const ROUTES = {
-    HOSTNAME: 'https://fusoras.is-a.dev',
-    REPOSITORY: 'https://github.com/fusoras',
-    GITHUB_PAGES: 'https://fusoras.github.io',
-    LINKEDIN: 'https://linkedin.com/in/fusoras',
+  HOSTNAME: 'https://fusoras.is-a.dev',
+  REPOSITORY: 'https://github.com/fusoras',
+  GITHUB_PAGES: 'https://fusoras.github.io',
+  LINKEDIN: 'https://linkedin.com/in/fusoras',
 } as const
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,11 @@
 import es from '@/i18n/es.json'
 import en from '@/i18n/en.json'
 
-export const getI18N = ({	currentLocale = 'es',}: {currentLocale: string | undefined;}) => {
-	if (currentLocale === 'en') return {...es, ...en};
-	return es;
-};
+export const getI18N = ({
+  currentLocale = 'es',
+}: {
+  currentLocale: string | undefined
+}) => {
+  if (currentLocale === 'en') return { ...es, ...en }
+  return es
+}
